a71f9cdd49d62f77e4144a121d45fcef4bdd5a04
Alsweider <30653982+Alsweider@users.noreply.github.com>
2023-05-27 18:05:34 +0200

    Update SpotiRagequit.bat

--- SpotiRagequit.bat
+++ SpotiRagequit.bat
@@ -1,9 +1,6 @@
-title SpotiRagequit 0.0.1
-taskkill /f /im Spotify.exe
-taskkill /f /im Spotify.exe
-taskkill /f /im Spotify.exe
-taskkill /f /im Spotify.exe
+title SpotiRagequit v0.0.1
 taskkill /f /im Spotify.exe
 timeout 1
 start %appdata%\Spotify\Spotify.exe
-echo ^{SPACE} | powershell -c "$wshell = New-Object -ComObject wscript.shell; $wshell.AppActivate('Spotify'); $wshell.SendKeys(' ')"
+timeout 1
+powershell -c "$wshell = New-Object -ComObject wscript.shell; $wshell.AppActivate('Spotify'); $wshell.SendKeys(' ')"
